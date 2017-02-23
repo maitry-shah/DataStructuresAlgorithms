13fa0ec4c9047fa526c1b55b1b54f8674a4c4443
maitry-shah <smai3.shah@gmail.com>
2017-02-22 18:44:40 -0800

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,20 +3,25 @@ public class BubbleSort{
 
     public void Sort(int myarray[]){
         int temp=0;
+       for(int j=1;j<myarray.length;j++){ 
     for(int i=1;i<myarray.length;i++){
-        if(myarray[i-1]>myarray[i])
+        if(myarray[i-1]>myarray[i]){
         
-        temp=myarray[i-1];
-        myarray[i]=myarray[i-1];
-        myarray[i-1]=temp;
+        temp=myarray[i];
+     
+        myarray[i]= myarray[i-1];
+           myarray[i-1]=temp;
     }
+    
+    }
+       }
     
     }
 
     public static void main(String args[]){
     
-   int[] myarray={9,34,5,76,12,1};
-        InsertionSort sortObj=new InsertionSort();
+   int[] myarray={45,34,23,12,10,1};
+        BubbleSort sortObj=new BubbleSort();
         sortObj.Sort(myarray);
         for(int i=0;i<myarray.length;i++){
         System.out.println(myarray[i]);
